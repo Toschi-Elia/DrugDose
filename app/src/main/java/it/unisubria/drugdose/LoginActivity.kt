ac732e99c7e1d0ff7ed06c933c52086e0e502068
elia <eliatoschi.24@gmail.com>
2026-04-29 19:48:50 +0200

    modifica login

--- app/src/main/java/it/unisubria/drugdose/LoginActivity.kt
+++ app/src/main/java/it/unisubria/drugdose/LoginActivity.kt
@@ -1,11 +1,22 @@
 package it.unisubria.drugdose
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val buttonOspite= findViewById<Button>(R.id.btnAccediOspite)
+
+        buttonOspite.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
