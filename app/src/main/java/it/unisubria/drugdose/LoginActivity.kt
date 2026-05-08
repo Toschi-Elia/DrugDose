d64820707ba30c8eb83fefa180fdc7683e910884
elia <eliatoschi.24@gmail.com>
2026-05-08 18:55:05 +0200

    Scrittura registrazione e accesso

--- app/src/main/java/it/unisubria/drugdose/LoginActivity.kt
+++ app/src/main/java/it/unisubria/drugdose/LoginActivity.kt
@@ -8,36 +8,54 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val bottoneOspite= findViewById<Button>(R.id.btnAccediOspite)
-        val bottoneRegistrati= findViewById<Button>(R.id.btnRegistrati)
-        val textPasswordDimenticata= findViewById<TextView>(R.id.tvPasswordDimenticata)
-        val bottoneAccedi= findViewById<Button>(R.id.btn_login)
+        val bottoneOspite = findViewById<Button>(R.id.btnAccediOspite)
+        val bottoneRegistrati = findViewById<Button>(R.id.btnRegistrati)
+        val textPasswordDimenticata = findViewById<TextView>(R.id.tvPasswordDimenticata)
+        val bottoneAccedi = findViewById<Button>(R.id.btn_login)
 
-        bottoneOspite.setOnClickListener{
+        var authRepo = AuthRepository()
+        bottoneOspite.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
 
         }
 
-        bottoneRegistrati.setOnClickListener{
-            val intent= Intent(this, RegisterActivity::class.java)
+        bottoneRegistrati.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
 
-        bottoneAccedi.setOnClickListener{
-            Toast.makeText(this,"btn accedi", Toast.LENGTH_SHORT).show()
+        bottoneAccedi.setOnClickListener {
+            Toast.makeText(this, "btn accedi", Toast.LENGTH_SHORT).show()
+            val textMail = findViewById<TextView>(R.id.text_mail)
+            val textPsw = findViewById<TextView>(R.id.text_password)
+            authRepo.eseguiLogin(
+                textMail.text.toString(),
+                textPsw.text.toString()
+            ) { successo, errore ->
+                if (successo) {
+                    Toast.makeText(this, "Accesso", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+
+                } else {
+                    Toast.makeText(this, "Errore $errore", Toast.LENGTH_SHORT).show()
+                    //TODO aggiunta cambio colore
+                }
+            }
+
+            textPasswordDimenticata.setOnClickListener {
+                Toast.makeText(this, "tb password dimenticata", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        textPasswordDimenticata.setOnClickListener {
-            Toast.makeText(this, "tb password dimenticata", Toast.LENGTH_SHORT).show()
-        }
+
     }
-
-
 }
 
 
--- app/src/main/java/it/unisubria/drugdose/RegisterActivity.kt
+++ app/src/main/java/it/unisubria/drugdose/RegisterActivity.kt
@@ -3,9 +3,11 @@ package it.unisubria.drugdose
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -19,19 +21,38 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var authRepo = AuthRepository()
         val bottoneOspite = findViewById<Button>(R.id.btnAccediOspite)
         val bottoneAccedi= findViewById<Button>(R.id.btnAccedi)
         val bottoneRegistrati= findViewById<Button>(R.id.btn_registrati)
+
+        val textNome= findViewById<TextView>(R.id.text_nome).text.toString()
+        val textCognome= findViewById<TextView>(R.id.text_cognome).text.toString()
+
+
 
 
         bottoneOspite.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-
         }
 
         bottoneRegistrati.setOnClickListener{
-            Toast.makeText(this, "Registrazione", Toast.LENGTH_SHORT).show()
+            val textMail= findViewById<TextView>(R.id.text_mail)
+            val textPsw= findViewById<TextView>(R.id.text_password)
+            authRepo.registraUtente(textMail.text.toString(),textPsw.text.toString()){
+                successo, errore -> if(successo) {
+                    Toast.makeText(this, "Registrazione effettuata", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+
+                }else{
+                    Toast.makeText(this, "Errore $errore",Toast.LENGTH_SHORT).show()
+                    textMail.setTextColor(ContextCompat.getColor(this, android.R.color.holo_red_dark))
+                }
+            }
+
         }
 
         bottoneAccedi.setOnClickListener{
@@ -41,4 +62,10 @@ class RegisterActivity : AppCompatActivity() {
 
 
     }
+    fun verificaPassword(str: String): Boolean {
+
+return true
+    }
+
+
 }
