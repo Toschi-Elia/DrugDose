323d9da9be3b5ef8504ecb89d1d5ef53cced3270
vis3ee <visellimichele2@gmail.com>
2026-05-12 16:21:39 +0200

    Aggiunta dipendenza firebase firestore
    Aggiunto in gradle Cloud Firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
 }
