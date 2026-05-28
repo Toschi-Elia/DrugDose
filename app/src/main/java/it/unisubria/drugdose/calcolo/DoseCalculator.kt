efed0c34f196ded4fc4ab08ff63ed0622658c6cb
vis3ee <visellimichele2@gmail.com>
2026-05-28 11:32:42 +0200

    Aggiornato DoseCalculator.kt
    Aggiunta seconda funzione base di calcolo ("calcoloRangeDosePerKg") e funzioni per controllare età ("etaCompatibile") e peso ("pesoCompatibile") richiesto

--- app/src/main/java/it/unisubria/drugdose/calcolo/DoseCalculator.kt
+++ app/src/main/java/it/unisubria/drugdose/calcolo/DoseCalculator.kt
@@ -5,4 +5,28 @@ object DoseCalculator {
     fun calcolaDosePerKg(dosePerKg: Double, pesoKg: Double): Double {
         return dosePerKg * pesoKg
     }
+
+    fun calcolaRangeDosePerKg(
+        dosePerKgMin: Double,
+        dosePerKgMax: Double,
+        pesoKg: Double
+    ): Pair<Double, Double> {
+        val doseMin = dosePerKgMin * pesoKg
+        val doseMax = dosePerKgMax * pesoKg
+        return Pair(doseMin, doseMax)
+    }
+
+    fun etaCompatibile(etaAnni: Int, etaMin: Int, etaMax: Int): Boolean {
+        return etaAnni in etaMin..etaMax
+    }
+
+    fun pesoCompatibile(
+        pesoKg: Double,
+        pesoMinKg: Double?,
+        pesoMaxKg: Double?
+    ): Boolean {
+        val sopraMinimo = pesoMinKg == null || pesoKg >= pesoMinKg
+        val sottoMassimo = pesoMaxKg == null || pesoKg < pesoMaxKg
+        return sopraMinimo && sottoMassimo
+    }
 }
